7763fba16950d8fc71ce28404eb8dec6518f4d75
tilld4wn <pjristeca@gmail.com>
2019-03-01 15:24:18 -0600

    new stuff

--- ecoli_proj.py
+++ ecoli_proj.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 import os
+import sys
 from Bio import SeqIO
-
-
-
-#Set cwd to the current directory that you are in
+# Set cwd to the current directory that you are in
 cwd = os.getcwd()
 print(cwd)
 #Create a new directory named 'FirstName_LastName'
@@ -14,128 +12,197 @@ if(not os.path.exists('Paul_Risteca')):
 else:
     os.chdir('Paul_Risteca')
 
-# Create some storage for each strain
-HM27 = ('ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/825/GCF_000387825.2_ASM38782v2/GCF_000387825.2_ASM38782v2_genomic.fna.gz','ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/825/GCF_000387825.2_ASM38782v2/GCF_000387825.2_ASM38782v2_feature_count.txt.gz')
-HM46 = ('ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/845/GCF_000387845.2_ASM38784v2/GCF_000387845.2_ASM38784v2_genomic.fna.gz','ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/845/GCF_000387845.2_ASM38784v2/GCF_000387845.2_ASM38784v2_feature_count.txt.gz')
-HM65 = ('ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/785/GCF_000387785.2_ASM38778v2/GCF_000387785.2_ASM38778v2_genomic.fna.gz','ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/785/GCF_000387785.2_ASM38778v2/GCF_000387785.2_ASM38778v2_feature_count.txt.gz')
-HM69 = ('ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/865/GCF_000387865.2_ASM38786v2/GCF_000387865.2_ASM38786v2_genomic.fna.gz','ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/865/GCF_000387865.2_ASM38786v2/GCF_000387865.2_ASM38786v2_feature_count.txt.gz')
+# ftp address lists
+hm27_ftp = ['hm27']
+hm46_ftp = ['hm46']
+hm65_ftp = ['hm65']
+hm69_ftp = ['hm69']
 
+# stores strain name, fasta file, feature count file, prokka output file prefix, SRA accession, tophat output prefix
+hm27_data = ['hm27', 'hm27_fasta.fna', 'hm27_feature_count.txt', 'hm27_prokka', 'SRR1278956', 'hm27_tophat_out', 'hm27_cufflinks_out']
+hm46_data = ['hm46', 'hm46_fasta.fna', 'hm46_feature_count.txt', 'hm46_prokka', 'SRR1278960', 'hm46_tophat_out', 'hm27_cufflinks_out']
+hm65_data = ['hm65', 'hm65_fasta.fna', 'hm65_feature_count.txt', 'hm65_prokka', 'SRR1283106', 'hm65_tophat_out', 'hm27_cufflinks_out']
+hm69_data = ['hm69', 'hm69_fasta.fna', 'hm69_feature_count.txt', 'hm69_prokka', 'SRR1278963', 'hm69_tophat_out', 'hm27_cufflinks_out']
 
-os.system("wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/825/GCF_000387825.2_ASM38782v2/GCF_000387825.2_ASM38782v2_genomic.fna.gz -O HM27_fasta.fna.gz")
-os.system("wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/825/GCF_000387825.2_ASM38782v2/GCF_000387825.2_ASM38782v2_feature_count.txt.gz -O HM27_feature_count.txt.gz")
-os.system("wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/845/GCF_000387845.2_ASM38784v2/GCF_000387845.2_ASM38784v2_genomic.fna.gz -O HM46_fasta.fna.gz")
-os.system("wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/845/GCF_000387845.2_ASM38784v2/GCF_000387845.2_ASM38784v2_feature_count.txt.gz -O HM46_feature_count.txt.gz")
-os.system("wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/785/GCF_000387785.2_ASM38778v2/GCF_000387785.2_ASM38778v2_genomic.fna.gz -O HM65_fasta.fna.gz")
-os.system("wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/785/GCF_000387785.2_ASM38778v2/GCF_000387785.2_ASM38778v2_feature_count.txt.gz -O HM65_feature_count.txt.gz")
-os.system("wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/865/GCF_000387865.2_ASM38786v2/GCF_000387865.2_ASM38786v2_genomic.fna.gz -O HM69_fasta.fna.gz")
-os.system("wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/387/865/GCF_000387865.2_ASM38786v2/GCF_000387865.2_ASM38786v2_feature_count.txt.gz -O HM69_feature_count.txt.gz")
+# stores location of prokka .txt output and other important files
+hm27_prokka = ['./hm27_prokka/hm27_prokka.txt', './hm27_prokka/hm27_prokka.gff']
+hm46_prokka = ['./hm46_prokka/hm46_prokka.txt', './hm46_prokka/hm46_prokka.gff']
+hm65_prokka = ['./hm65_prokka/hm65_prokka.txt', './hm65_prokka/hm65_prokka.gff']
+hm69_prokka = ['./hm69_prokka/hm69_prokka.txt', './hm69_prokka/hm69_prokka.gff']
 
-os.system("prefetch SRR1278956")
-os.system("prefetch SRR1278960")
-os.system("prefetch SRR1283106")
-os.system("prefetch SRR1278963")
+# Stores location of important tophat files
+hm27_tophat = ['./hm27_tophat_out/accepted_hits.bam']
+hm46_tophat = ['./hm46_tophat_out/accepted_hits.bam']
+hm65_tophat = ['./hm65_tophat_out/accepted_hits.bam']
+hm69_tophat = ['./hm69_tophat_out/accepted_hits.bam']
 
-os.system("gunzip -d *.gz")
-# SRA File addresses
-HM27 = ftp://ftp.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR127/SRR1278956/SRR1278956.sra
-HM46 = ftp://ftp.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR127/SRR1278960/SRR1278960.sra
-HM65 = ftp://ftp.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR128/SRR1283106/SRR1283106.sra
-HM69 = ftp://ftp.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR127/SRR1278963/SRR1278963.sra
-#
-HM27 = ("HM27_fasta.fna","HM27_feature_count.txt","HM27", 'hm27_prokka', "./hm27_prokka/hm27_prokka.txt", "SRR1278956","./hm27_prokka/hm27_prokka.gff","./HM27_tophat_out/accepted_hits.bam")
-HM46 = ("HM46_fasta.fna","HM46_feature_count.txt","HM46", 'hm46_prokka'," ","SRR1278960","./hm46_prokka/hm46_prokka.gff","./HM46_tophat_out/accepted_hits.bam")
-HM65 = ("HM65_fasta.fna","HM65_feature_count.txt","HM65", 'hm65_prokka'," ","SRR1283106","./hm65_prokka/hm65_prokka.gff","./HM65_tophat_out/accepted_hits.bam")
-HM69 = ("HM69_fasta.fna","HM69_feature_count.txt","HM69", 'hm69_prokka'," ","SRR1278963","./hm65_prokka/hm65_prokka.gff","./HM69_tophat_out/accepted_hits.bam")
+hm27_cuff = ['./hm27_cufflinks_out/transcripts.gtf\n']
+hm46_cuff = ['./hm46_cufflinks_out/transcripts.gtf\n']
+hm65_cuff = ['./hm65_cufflinks_out/transcripts.gtf\n']
+hm69_cuff = ['./hm69_cufflinks_out/transcripts.gtf\n']
 
-# 2&3
-def recordInfo(strain,log_file):
-    record = list(SeqIO.parse(strain[0],"fasta"))
-    number_of_contigs = len(record)
-    log_file.write("There are "+str(number_of_contigs)+" contigs in the assembly "+strain[2]+". \n")
-    count = 0
-    for read in record:
-        if len(read.seq) > 1000:
-            count += len(read.seq)
-    log_file.write("There are "+str(count)+" bp in the assembly "+strain[2]+". \n")
+# Retrieve ftp addresses from input file "sample_data.txt"
+def get_ftp(strain, data_file):
+    # with open('sample_data.txt','r') as data_file:
+    for line in data_file:
+        temp = line.strip('\n').split('\t')
+        if temp[0] == strain[0]:
+            strain.append(temp[1])
+            strain.append(temp[2])
     return
 
-# # prokka
-def annotaion_prokka(prokka_prefix, strain_fasta,log_file):
+# Pulls data from NCBI using wget and ftp addresses
+def download_data(strain):
+    wget_command_fasta = "wget {} -O {}".format(strain[1], strain[0]+'_fasta.fna.gz')
+    wget_command_feature_content = "wget {} -O {}".format(strain[2], strain[0]+'_feature_count.txt.gz')
+    os.system(wget_command_fasta)
+    os.system(wget_command_feature_content)
+    return
 
-    prokka_command = "prokka --usegenus --genus Escherichia --cpus 4 --prefix {} {}".format(prokka_prefix, strain_fasta)
-    log_file.write(prokka_command+"\n")
-    os.system(prokka_command)
+# Reads the fasta file in and determines number of contigs present and length of assembly when all reads longer than 1000 bp
+def seqCount(data_lst, log_file):
+    for strain in data_lst:
+        record = list(SeqIO.parse(strain[1], 'fasta'))
+        contigs = str(len(record))
+        log_file.write("There are {} contigs in the assembly {}.\n".format(contigs, strain[0]))
+        count = 0
+        for read in record:
+            if len(read.seq) > 1000:
+                count += len(read.seq)
+        log_file.write("There are {} bp in the assembly {}.\n".format(str(count), strain[0]))
+    return
 
-def tophat_cufflinks(strain_fasta, read1, read2, log_file):
-    fasta_copy = "cp {} {}.fa".format(strain_fasta[0], strain_fasta[2])
-    bowt2_command = "bowtie2-build --threads 6 -f {} {}".format(strain_fasta[0], strain_fasta[2])
-    tophat_command = "tophat -p 6 -o {} {} {} {}".format(strain_fasta[2]+"_tophat_out",strain_fasta[2],read1,read2)
-    os.system(bowt2_command)
-    os.system(fasta_copy)
-    log_file.write(tophat_command+"\n")
-    os.system(tophat_command)
+def prokka_run(data_lst, prokka_lst, log_file):
+    log_file.write("Now running prokka.\n")
+    for strain in data_lst:
+        prokka_command = "prokka --usegenus --genus Escherichia --cpus 4 --prefix {} {}".format(strain[3], strain[1])
+        log_file.write(prokka_command+'\n')
+        os.system(prokka_command)
 
-def cufflinks(gff_file,cuff_out,bam,strain):
-    sort_bam_command = "samtools sort {} -o {}".format(strain[7], strain[2]+'_bam')
-    os.system(sort_bam_command)
-    cufflink_command = "cufflinks -p 6 -G {} -o {} {}".format(gff_file, cuff_out, strain[2]+'_bam')
-    os.system(cufflink_command)
+    for strain, prokInfo in zip(data_lst, prokka_lst):
+        feature_count = {"CDS":0, "tRNA":0}
+        prokka_count = {"CDS":0, "tRNA":0}
+        with open(strain[2], "r") as in_file:
+            line_arr = []
+            for line in in_file:
+                line_arr.append(line)
 
-def cuffmerge_norm(assembly_file, merged_gtf, strain1, strain2, strain3, strain4):
-    # cuffmerge_command = "cuffmerge -p 6 -o {} {}".format('merged_ecoli', assembly_file)
-    # os.system(cuffmerge_command)
-    cuffnorm_command = "cuffnorm -o diff_results -p 12 {} {} {} {}".format(merged_gtf, strain1[2]+'_bam', strain2[2]+'_bam', strain3[2]+'_bam', strain4[2]+'_bam')
+            for line in line_arr:
+                temp = line.strip('\n').split('\t')
+
+                if temp[0] == "CDS" and temp[1] == "with_protein":
+                    feature_count["CDS"] = int(temp[6])
+
+                if temp[0] == "tRNA":
+                    feature_count["tRNA"] = int(temp[6])
+
+        with open(prokInfo[0], "r") as in_file:
+            line_arr = []
+            for line in in_file:
+                line_arr.append(line)
+
+            for line in line_arr:
+                temp = line.strip('\n').split(':')
+
+                if temp[0] == "CDS":
+                    prokka_count["CDS"] = int(temp[1])
+
+                if temp[0] == "tRNA":
+                    prokka_count["tRNA"] = int(temp[1])
+        diffCDS = feature_count["CDS"]-prokka_count["CDS"]
+        diffT = feature_count["tRNA"]-prokka_count["tRNA"]
+        if diffCDS > 0 and diffT > 0:
+            log_file.write("Prokka found {} less CDS and {} less tRNA than the RefSeq in assembly {}.\n".format(abs(diffCDS), abs(diffT), strain[0]))
+        elif diffCDS > 0 and diffT <= 0:
+            log_file.write("Prokka found {} less CDS and {} more tRNA than the RefSeq in assembly {}.\n".format(abs(diffCDS), abs(diffT), strain[0]))
+        elif diffCDS <= 0 and diffT > 0:
+            log_file.write("Prokka found {} more CDS and {} less tRNA than the RefSeq in assembly {}.\n".format(abs(diffCDS), abs(diffT), strain[0]))
+        elif diffCDS <= 0 and diffT <= 0:
+            log_file.write("Prokka found {} more CDS and {} more tRNA than the RefSeq in assembly {}.\n".format(abs(diffCDS), abs(diffT), strain[0]))
+    return
+
+# Run TopHat for all assemblies
+def tophat_run(data_lst, log_file):
+    #runs bowtie2 to produce index files for Tophat2
+    log_file.write("Now running BowTie2.\n")
+    for strain in data_lst:
+        bowt2_command = "bowtie2-build --threads 4 -f {} {}".format(strain[1], strain[0])
+        os.system(bowt2_command)
+    log_file.write("Now running Tophat2.\n")
+    for strain in data_lst:
+        tophat_command = "tophat -p 4 -o {} {} {} {}".format(strain[5], strain[0], strain[4]+"_1.fastq", strain[4]+"_2.fastq")
+        os.system(tophat_command)
+    return
+
+# Run Cufflinks on sample data
+def cuff_run(data_lst, prokka_lst, tophat_lst):
+    for strain, prok, top in zip(data_lst, prokka_lst, tophat_lst):
+        cuff_command = "cufflinks -p 4 -G {} -o {} {}".format(prok[1], strain[6], top[0])
+        os.system(cuff_command)
+    return
+
+# Run Cuffmerge and cuff norm
+def cuffmerge_run(assembly, merged_gtf, hm27, hm46, hm65, hm69):
+    cuffmerge_command = "cuffmerge -p 4 -o {} {}".format('merged', assembly)
+    os.system(cuffmerge_command)
+    cuffnorm_command = "cuffnorm -o cuffnorm_results -p 4 {} {} {} {}".format(merged_gtf, hm27[0], hm46[0], hm65[0], hm69[0])
     os.system(cuffnorm_command)
-# def Record(strain):
-#     recordSeqIO.parse(strain[0],"fasta"):
+
+    return
 def main():
-    output_file = open ("UPEC.log","a")
-    recordInfo(HM27,output_file)
-    recordInfo(HM46,output_file)
-    recordInfo(HM65,output_file)
-    recordInfo(HM69,output_file)
+    log_file = open("UPEC.log", "a")
 
-    # # testing with hm27
-    annotaion_prokka(HM27[3],HM27[0],output_file)
-    annotaion_prokka(HM46[3],HM46[0],output_file)
-    annotaion_prokka(HM65[3],HM65[0],output_file)
-    annotaion_prokka(HM69[3],HM69[0],output_file)
+    data_file = []
+    for data in sys.stdin:
+        data_file.append(data)
 
-    # # try to see if this will add to UPEC
-    os.system("cat ./hm27_prokka/hm27_prokka.txt >> UPEC.log")
-    os.system("cat ./hm46_prokka/hm46_prokka.txt >> UPEC.log")
-    os.system("cat ./hm65_prokka/hm65_prokka.txt >> UPEC.log")
-    os.system("cat ./hm69_prokka/hm69_prokka.txt >> UPEC.log")
-    #
-    os.system("prefetch SRR1278956")
-    os.system("prefetch SRR1278960")
-    os.system("prefetch SRR1283106")
-    os.system("prefetch SRR1278963")
-    os.system("fastq-dump -I --split-files ~/ncbi/public/sra/SRR1278956.sra")
-    os.system("fastq-dump -I --split-files ~/ncbi/public/sra/SRR1278960.sra")
-    os.system("fastq-dump -I --split-files ~/ncbi/public/sra/SRR1283106.sra")
-    os.system("fastq-dump -I --split-files ~/ncbi/public/sra/SRR1278963.sra")
-    #
-    tophat_cufflinks(HM27,HM27[5]+"_1.fastq", HM27[5]+"_2.fastq",output_file)
-    tophat_cufflinks(HM46,HM46[5]+"_1.fastq", HM46[5]+"_2.fastq",output_file)
-    tophat_cufflinks(HM65,HM65[5]+"_1.fastq", HM65[5]+"_2.fastq",output_file)
-    tophat_cufflinks(HM69,HM69[5]+"_1.fastq", HM69[5]+"_2.fastq",output_file)
+# Getting data into working directory to begin analysis
+    ftp_lst = [hm27_ftp, hm46_ftp, hm65_ftp, hm69_ftp]
 
-    cufflinks(HM27[6],HM27[2],HM27[7],HM27)
-    cufflinks(HM46[6],HM46[2],HM46[7],HM46)
-    cufflinks(HM65[6],HM65[2],HM65[7],HM65)
-    cufflinks(HM69[6],HM69[2],HM69[7],HM69)
+    for strain in ftp_lst:
+        get_ftp(strain, data_file)
 
-    with open('ecoli_assemblies.txt', 'w') as assemble_file:
-        assemble_file.write("./HM27/transcripts.gtf\n")
-        assemble_file.write("./HM46/transcripts.gtf\n")
-        assemble_file.write("./HM65/transcripts.gtf\n")
-        assemble_file.write("./HM69/transcripts.gtf\n")
+    for strain_info in ftp_lst:
+        download_data(strain_info)
 
-    cuffmerge_norm('ecoli_assemblies.txt', './merged_ecoli/merged.gtf', HM27, HM46, HM65, HM69)
-    output_file.close()
+    os.system("gunzip -d *.gz")
 
+# Finding number of contigs and assembly length
+    data_lst = [hm27_data, hm46_data, hm65_data, hm69_data]
+    seqCount(data_lst, log_file)
 
+# Running Prokka on data
+    prokka_lst = [hm27_prokka, hm46_prokka, hm65_prokka, hm69_prokka]
+    prokka_run(data_lst, prokka_lst, log_file)
+
+    for strain, prokInfo in zip(data_lst, prokka_lst):
+        log_file.write("Prokka output for {}:\n".format(strain[0]))
+        file = prokInfo[0]
+        with open(file, 'r') as input_file:
+            for line in input_file:
+                log_file.write(line+'\n')
+
+# Running Tophat2
+    for sra in data_lst:
+        prefetch_command = "prefetch {}".format(sra[4])
+        os.system(prefetch_command)
+    for sra in data_lst:
+        fastqdump_cmnd = "fastq-dump -I --split-files ~/ncbi/public/sra/{}.sra".format(sra[4])
+        os.system(fastqdump_cmnd)
+    tophat_run(data_lst, log_file)
+
+# Running cufflinks
+    tophat_lst = [hm27_tophat, hm46_tophat, hm65_tophat, hm69_tophat]
+    cuff_run(data_lst, prokka_lst, tophat_lst)
+
+# Running cuffmerge and cuffnorm
+    merged_gtf = "./merged/merged.gtf"
+    cuff_lst = [hm27_cuff, hm46_cuff, hm65_cuff, hm69_cuff]
+    with open('assemblies.txt', 'w') as out_file:
+        for gtf in cuff_lst:
+            out_file.write(gtf[0])
+
+    cuffmerge_run('assemblies.txt', merged_gtf, hm27_tophat, hm46_tophat, hm65_tophat, hm69_tophat)
 
 if __name__ == '__main__':
     main()
